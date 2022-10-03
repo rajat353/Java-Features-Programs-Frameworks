72f19a9c08275079e537eb23b885caa106178461
RJ353 <72256036+RJ353@users.noreply.github.com>
2022-10-04 05:17:07 +0530

    Create validParentheses.java

---
+++ Core Java/Programming/validParentheses.java
@@ -0,0 +1,25 @@
+class ValidParentheses {
+    public static void main(String args[]) {
+        String s="({})[]";
+        Stack<Character> st= new Stack<>();
+        
+        for(char c: s.toCharArray()){
+            if(c=='(')
+                st.push(')');
+            else if(c=='[')
+                st.push(']');
+            else if(c=='{')
+                st.push('}');
+            else if(st.empty() || st.pop()!=c){
+              { 
+                System.out.println("Not Valid");
+                return;
+              }
+            }
+            
+        }
+        
+        if(st.empty()) System.out.println("Valid");
+        else System.out.println("Not Valid"); 
+    }
+}
